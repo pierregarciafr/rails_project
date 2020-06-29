f6fcf748e275dc61c354e8298a6df3cc656a0fc9
Pierre Garcia <pg.pierre.garcia@free.fr>
2020-06-29 17:37:20 +0200

    implement show for user and asso with link from index

--- app/controllers/assos_controller.rb
+++ app/controllers/assos_controller.rb
@@ -3,4 +3,8 @@ class AssosController < ApplicationController
   def index
     @assos = Asso.all
   end
+
+  def show
+    @asso = Asso.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :members, controllers: { sessions: 'members/sessions' }
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [ :index ]
-  resources :assos, only: [ :index ]
+  resources :users, only: [ :index, :show ]
+  resources :assos, only: [ :index, :show ]
 
 end
